877a01e5b1ea2bee1a16193bd62b4015fe8bd93e
Rodrigo Palenske <rodrigopalenske@gmail.com>
2025-05-30 13:46:27 -0300

    Adequando service paciente e configurando banco
    Adequando service de pacientes para permitir que um paciente não necessite ter uma agenda para ser criado. Configurando banco de dados

--- projectClinic/src/main/java/sweet/dreams/projectClinic/service/PacienteService.java
+++ projectClinic/src/main/java/sweet/dreams/projectClinic/service/PacienteService.java
@@ -2,17 +2,16 @@ package sweet.dreams.projectClinic.service;
 
 
 import jakarta.transaction.Transactional;
+import jakarta.validation.ConstraintViolationException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import sweet.dreams.projectClinic.model.Paciente;
 import sweet.dreams.projectClinic.repository.PacienteRepository;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class PacienteService {
-
     @Autowired
     private PacienteRepository repository;
 
@@ -23,10 +22,6 @@ public class PacienteService {
             if (paciente.getCpf().isBlank() || paciente.getCpf().isEmpty()) throw new RuntimeException("Adicione o CPF do Paciente");
             if (paciente.getDataNascimento().isBlank() || paciente.getDataNascimento().isEmpty()) throw new RuntimeException("Adicione a data de nascimento do Paciente");
             if (paciente.getTelefone().isBlank() || paciente.getTelefone().isEmpty()) throw new RuntimeException("Adicione o telefone do Paciente");
-
-            if (paciente.getAgendas().isEmpty()) paciente.setAgendas(new ArrayList<>());
-            if (!paciente.getAgendas().isEmpty())
-                paciente.getAgendas().forEach(agenda -> agenda.setPaciente(paciente));
 
             repository.save(paciente);
         } catch (Exception e) {
