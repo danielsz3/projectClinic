c4b084df1c2f50c3ea676c375a702f238402a073
Gustavo Passos <143558941+GustavoPS18@users.noreply.github.com>
2025-05-30 20:02:06 -0300

    Correções

--- projectClinic-gustavo/projectClinic/src/main/java/sweet/dreams/projectClinic/repository/AgendaRepository.java
+++
@@ -1,7 +0,0 @@
-package sweet.dreams.projectClinic.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import sweet.dreams.projectClinic.model.Agenda;
-
-public interface AgendaRepository extends JpaRepository<Agenda, Long> {
-}
--- projectClinic-gustavo/projectClinic/src/main/java/sweet/dreams/projectClinic/repository/PacienteRepository.java
+++
@@ -1,7 +0,0 @@
-package sweet.dreams.projectClinic.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import sweet.dreams.projectClinic.model.Paciente;
-
-public interface PacienteRepository extends JpaRepository<Paciente, Long> {
-}
